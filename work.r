df1882c9616e7c24771a773309314463d0d8ac18
EricRiney <fails.to.install@gmail.com>
2016-11-12 21:28:58 -0800

    two ﬁles and two constants

--- work.r
+++ work.r
@@ -7,6 +7,9 @@ linkData  = read.table("370_Assign2_App_data/pubmed_links_INFO370.txt", sep = "\
 View(nodeData)  # allows me to view the data in nodeData
 View(linkData)  # allows me to view the data in linkData
 
+Alpha = 0.85
+Epsilon = 0.00001
+
 # sort(table(linkData$cited),decreasing=TRUE)[1:3]    # returns the ID of the top 3 most cited papers
 # sort(table(nodeData$journal),decreasing=TRUE)[1:3]  # returns the ID of the top 3 most cited journals
 
