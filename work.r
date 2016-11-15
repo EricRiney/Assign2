448e2d7b481fc6eaae4b2bf673bbc59c19b7842c
EricRiney <fails.to.install@gmail.com>
2016-11-15 13:33:26 -0800

    notes

--- work.r
+++ work.r
@@ -7,11 +7,12 @@ linkData  = read.table("370_Assign2_App_data/pubmed_links_INFO370.txt", sep = "\
 View(nodeData)  # allows me to view the data in nodeData
 View(linkData)  # allows me to view the data in linkData
 
-Alpha = 0.85      # constant
-Epsilon = 0.00001 # constant
+Alpha   = 0.85      # constant
+Epsilon = 0.00001   # constant
 
 # sort(table(linkData$cited),decreasing=TRUE)[1:3]    # returns the ID of the top 3 most cited papers
 # sort(table(nodeData$journal),decreasing=TRUE)[1:3]  # returns the ID of the top 3 most cited journals
 
 # dates = as.numeric(nodeData$date_pub)   # converts dates to ints
 # hist(dates)                             # prints a histogram
+
