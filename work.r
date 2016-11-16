ba3eb3c14b1592cf39209b3f8a8333daa82caff3
EricRiney <fails.to.install@gmail.com>
2016-11-16 13:54:47 -0800

    hate

--- work.r
+++ work.r
@@ -10,6 +10,12 @@ View(linkData)  # allows me to view the data in linkData
 Alpha   = 0.85      # constant
 Epsilon = 0.00001   # constant
 
+data.a <- c(1, 0, 2, 0, 4, 3, 3, 0, 1, 1, 0, 0, 2, 0, 4, 0, 1, 0, 0, 0, 1, 0, 0, 1, 8, 0, 3, 0, 5, 2, 0, 0, 0, 0, 0, 0)
+matrix.a <- matrix(data.a, nrow = 6, ncol = 6, byrow = TRUE)
+matrix.a
+
+# P = Alpha * H +  
+
 # sort(table(linkData$cited),decreasing=TRUE)[1:3]    # returns the ID of the top 3 most cited papers
 # sort(table(nodeData$journal),decreasing=TRUE)[1:3]  # returns the ID of the top 3 most cited journals
 
